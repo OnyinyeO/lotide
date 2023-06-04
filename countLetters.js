93c48a9015341e23cf000e13d30d39ef997662d6
Onyinye Ochi <osemene.go@gmail.com>
2023-06-04 22:17:16 +0000

    Lotide assignment

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (allItem) => {
+  const result = {};
+  for (const item of allItem) {
+    if (item !== " ")
+      if (result[item]) {
+        result[item] += 1;
+      } else result[item] = 1;
+  }
+  return result;
+};
